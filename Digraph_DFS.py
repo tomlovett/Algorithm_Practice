cb93112b2f79f77077a65306ddc9edb8b414ef0f
Tom Lovett <tomlovett6@gmail.com>
2016-06-29 14:33:42 -0400

    digraph_DFS skeleton laid

--- Digraph_DFS.py
+++ Digraph_DFS.py
@@ -5,37 +5,58 @@
 
 class Digraph(object):
 	def __init__(self):
-		self.nodes = []
+		self.nodes = {}
 		self.all_paths     = {}
 		self.optimum_paths = {}
 
 	def add_node(self, node):
-		self.nodes.append(node)
+		self.nodes[node.name] = node
 
 	def add_edge(self, node_a, node_b, edge):
-		node_a.edges[node_b.name] = edge
+		node_a.edges[node_b.name] = edge  # assigning weight instead
 		node_b.edges[node_a.name] = edge
 
-	def depth_first_search(self, src, dest, path):
-		if src == dest:
+	def calc_weight(self, path):
+		if len(path) < 2:
+			return 0
+		weight = 0
+		for i in range(len(path)-1):
+			src, dest = path[i], path[i+1]
+			edge = src.edges[dest]
+			weight += edge.weight
+		return weight
+
+	def depth_first_search(self, src, dest, path, max_total):
+		if (src, dest) in self.memo:
+			return self.memo[(src, dest)]
+		elif src == dest:
 			return path
-			# record path
+
+		weight = self.calc_weight(path) # ready to expand to outside weight
+		if weight > max_total:
+			return
+
+		trials = []
 		for child in src.edges:
-			print child
 			if child in path:
 				continue
 			path.append(child)
-			# total_weight  += src.edges[child].weight
-			# total_outside += src.edges[child].outside_weight
-			self.depth_first_search(child, dest, path)
+			if self.depth_first_search(child, dest, path):
+				trials.append(path)
 			path.pop()
-		return 		# redundant? 
 
-	def record_path(self, path):
-		src  = path[0]
-		dest = path[-1]
-		if (src, dest) in self.paths:
-			other = self.paths
+		if len(trials) > 0:
+			shortest = trials[0]
+			for other in trials:
+				if self.calc_weight(other) < self.calc_weight(shortest):
+					shortest = other
+			self.memo[(src, dest)] = shortest
+			# careful, it might go around
+				# there may be a faster way that utilizes a node already in the path?
+				# my brain is farting
+			return shortest
+		else:
+			return
 
 class Node(object):
 	def __init__(self, name):
@@ -43,13 +64,7 @@ class Node(object):
 		self.edges = {}
 
 class Edge(object):
-	def __init__(self, outside_weight, weight):
+	def __init__(self, weight, outside_weight=0):
 		self.weight         = weight
-		self.outside_weight = outside_weight
+		# self.outside_weight = outside_weight
 
-class Path(object):
-	def __init__(self, src, dest):
-		self.src  = src
-		self.dest = dest
-		self.weight         = weight
-		self.outside_weight = outside_weight
